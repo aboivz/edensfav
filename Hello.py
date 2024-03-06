bb754ffdd88e906d29598e78620450c1d3b63d52
abo1vz <96767316+aboivz@users.noreply.github.com>
2024-03-07 01:36:14 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -11,8 +11,8 @@ file_uploaded = st.file_uploader('Choose the file', type = ['jpg', 'png', 'jpeg'
 if file_uploaded is not None:
         image = st.image(file_uploaded, caption=st.text('Casperz enemiez !!! :'))
 
-name = st.sidebar.text_area(label="What is the name of this person ?")
-gender = st.sidebar.text_area(label="tell me somthing about this man/woman ?")
+name = st.text_area(label="What is the name of this person ?")
+gender = st.text_area(label="tell me somthing about this man/woman ?")
 st.button("Reset", type="primary")
 if st.button('analyze') :
     result = lch.generate_stupid_names(name,gender)
