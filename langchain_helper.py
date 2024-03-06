69f5ee857b7136d0a559bff4ef36d56c466e442f
abo1vz <96767316+aboivz@users.noreply.github.com>
2024-03-07 00:57:27 +0700

    Add files via upload

---
+++ langchain_helper.py
@@ -0,0 +1,26 @@
+from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+import streamlit as st
+
+
+load_dotenv()
+
+def generate_stupid_names(name,gender):
+    llm = OpenAI(temperature=0.6)
+
+    prompt_template_name = PromptTemplate(
+        input_variables=['name','gender'],
+        template="Write a short roast to make fun of a person named {name} which is {gender}, make it ridiculous, stupid and funny of them that no one will like them. And tell them to stop"
+    )
+
+    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="names")
+
+    response = name_chain({'name': name, 'gender': gender})
+
+    return response
+
+if __name__ == "__main__":
+    print(generate_stupid_names("shitty, stupid, racist","cat"))
+   
