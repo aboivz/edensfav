d3d26800f9d3954ba48535e723803a8aaf2ca553
abo1vz <96767316+aboivz@users.noreply.github.com>
2024-03-07 01:39:23 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import os
 
 
-st.title("Casper's enemy roasting machine ")
+st.title("Casper's enemy roasting bot ")
 st.text("Upload an image of who is flirting u : ")
 
 file_uploaded = st.file_uploader('Choose the file', type = ['jpg', 'png', 'jpeg'])
@@ -13,7 +13,9 @@ if file_uploaded is not None:
 
 name = st.text_area(label="What is the name of this person ?")
 gender = st.text_area(label="tell me somthing about this man/woman ?")
+
+st.text("Bot's verdict :")
 st.button("Reset", type="primary")
-if st.button('analyze') :
+if st.button('Analyze') :
     result = lch.generate_stupid_names(name,gender)
     st.write(result)
