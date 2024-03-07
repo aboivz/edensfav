7194881b96e28727226a01075f8c9b3a286bf3a7
abo1vz <96767316+aboivz@users.noreply.github.com>
2024-03-07 20:35:25 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -5,7 +5,7 @@ import os
 
 
 st.title("Casper's enemy roasting bot ")
-st.text("Notice: This is an aggressive bot that will roast it's target, all of the bot's answers is AI trained with none of Casper's interferences. So I'm not responsible for any of the target's emotional damages. But if it's someone who is flirting with Eden. Obiviously, I don't care lol. ")
+st.text("Notice: This is an aggressive bot that will roast it's target,  \n all of the bot's answers is AI trained with none of Casper's interferences.  \n So I'm not responsible for any of the target's emotional damages.  \n But if it's someone who is flirting with Eden.  \n Obiviously, I don't care lol. ")
 
 st.text("Upload an image of who is flirting u Eden : ")
 
